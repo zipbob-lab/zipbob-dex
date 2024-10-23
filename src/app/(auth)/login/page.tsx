d6e42de19129170a197ecb7ed2c2122c01cc8e83
slppills <sunchaehoon2@naver.com>
2024-10-23 23:54:29 +0900

    add : 깃허브 로그인, 구글 로그인

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,16 +1,30 @@
-import KakaoButton from "@/components/common/button/KakaoButton";
-import { Metadata } from "next";
+"use client";
 
-export const metadata: Metadata = {
-  title: "로그인 페이지",
-  description: "로그인 페이지 입니다."
-};
+import GithubButton from "@/components/common/button/GithubButton";
+import GoogleButton from "@/components/common/button/GoogleButton";
+import KakaoButton from "@/components/common/button/KakaoButton";
+import LogoutButton from "@/components/common/button/LogoutButton";
+import browserClient from "@/supabase/client";
+import { useEffect } from "react";
+// import { Metadata } from "next";
+
+// export const metadata: Metadata = {
+//   title: "로그인 페이지",
+//   description: "로그인 페이지 입니다."
+// };
 
 const LoginPage = () => {
+  useEffect(() => {
+    browserClient.auth.getSession().then(console.log);
+  });
+
   return (
-    <>
+    <div className="flex flex-col">
       <KakaoButton />
-    </>
+      <GithubButton />
+      <GoogleButton />
+      <LogoutButton />
+    </div>
   );
 };
 
