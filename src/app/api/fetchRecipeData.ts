43e9d014419c9bd310bffc0d6f5d2e96c2a26af7
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-10-24 19:18:27 +0900

    add : api 불러오기 로직

---
+++ src/app/api/fetchRecipeData.ts
@@ -0,0 +1,21 @@
+export const fetchRecipeData = async () => {
+  const API_KEY = process.env.NEXT_PUBLIC_FOODRECIPE_API_KEY;
+  const API_URL = process.env.NEXT_PUBLIC_FOODRECIPE_API_URL;
+
+  if (!API_KEY || !API_URL) {
+    throw new Error("API key 또는 url을 불러오지 못했습니다.");
+  }
+
+  const response = await fetch(`${API_URL}/api/${API_KEY}/COOKRCP01/json/1/10`, {
+    method: "GET"
+  });
+
+  if (!response.ok) {
+    throw new Error("데이터를 불러오는데 실패했습니다.");
+  }
+
+  const data = await response.json();
+  console.log("데이터 불러오기 성공 / data 확인", data);
+  // console.log("데이터 불러오기 성공 / 조리법 확인", data.COOKRCP01.row);
+  return data;
+};
