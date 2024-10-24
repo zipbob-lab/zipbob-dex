9d7ca8d10e1466009c9aa365c54b7317d2609263
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-10-24 23:41:01 +0900

    add : 타입스크립트 작성 (임시)

---
+++ src/types/Recipe.ts
@@ -0,0 +1,12 @@
+export type Recipe = {
+  id: string;
+  RCP_NM: string; // 요리명
+  ATT_FILE_NO_MAIN: string; // 요리 이미지 (메인 이미지)
+  ATT_FILE_NO_MK: string; // 만드는 법 이미지
+  // MANUAL01: string; // 조리법 1단계
+  // MANUAL02: string; // 조리법 2단계
+  // MANUAL03: string; // 조리법 3단계
+  // MANUAL_IMG01: string; // 조리법 1단계 이미지
+  // MANUAL_IMG02: string; // 조리법 2단계 이미지
+  // MANUAL_IMG03: string; // 조리법 3단계 이미지
+};
