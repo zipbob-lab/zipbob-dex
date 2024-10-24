10074d4c81d119cc0f62e14e81a34784848a071b
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-10-24 23:43:38 +0900

    add : 음식 레시피 + 북마크 체크용 로직 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
-import BookmarkButton from "@/components/common/BookmarkButton";
+import { fetchRecipeData } from "@/app/api/fetchRecipeData";
+import RecipeCard from "@/components/RecipeCard";
+import { Recipe } from "@/types/Recipe";
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetchRecipeData();
+  const recipes = data.COOKRCP01.row.slice(0, 5); //5개만 썰어와
+
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Home 페이지 입니다..</h1>
+      <h1 className="text-2xl font-bold mb-4">요리 5개만 불러와보자</h1>
 
-      {/* 북마크 버튼 컴포넌트 사용 */}
-      <BookmarkButton recipeId="recipe_1" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {recipes.map((recipe: Recipe, index: number) => (
+          <RecipeCard
+            key={index}
+            recipe={{
+              id: recipe.id,
+              RCP_NM: recipe.RCP_NM,
+              ATT_FILE_NO_MAIN: recipe.ATT_FILE_NO_MAIN,
+              ATT_FILE_NO_MK: recipe.ATT_FILE_NO_MK
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 }
