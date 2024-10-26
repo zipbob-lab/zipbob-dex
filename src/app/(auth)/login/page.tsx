a48e9b60d468cbb070e0bec207117af275bc743e
slppills <sunchaehoon2@naver.com>
2024-10-26 14:26:13 +0900

    add : authPage

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,6 +5,7 @@ import GoogleButton from "@/components/common/button/GoogleButton";
 import KakaoButton from "@/components/common/button/KakaoButton";
 import LogoutButton from "@/components/common/button/LogoutButton";
 import browserClient from "@/supabase/client";
+import Link from "next/link";
 import { useEffect } from "react";
 // import { Metadata } from "next";
 
@@ -16,14 +17,23 @@ import { useEffect } from "react";
 const LoginPage = () => {
   useEffect(() => {
     browserClient.auth.getSession().then(console.log);
-  });
+  }, []);
 
   return (
-    <div className="flex flex-col">
-      <KakaoButton />
-      <GithubButton />
-      <GoogleButton />
-      <LogoutButton />
+    <div className="flex flex-col items-center">
+      <h1 className="text-[2rem]">일반 로그인</h1>
+      <form className="flex flex-col gap-4 mb-2">
+        <input type="email" placeholder="이메일 주소" className="border border-gray-500 rounded-md p-1" />
+        <input type="password" placeholder="비밀번호" className="border border-gray-500 rounded-md p-1" />
+        <button>로그인</button>
+      </form>
+      <Link href="/sign-up">회원가입</Link>
+      <div className="flex flex-col mt-8">
+        <KakaoButton />
+        <GithubButton />
+        <GoogleButton />
+        <LogoutButton />
+      </div>
     </div>
   );
 };
