eec2a9b08556fe92d2192e4478b779396f543676
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-10-26 17:35:49 +0900

    fix: db 데이터 불러오는 로직 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,17 @@
-import { fetchRecipeData } from "@/app/api/fetchRecipeData";
+import { fetchRecipeDbData } from "@/serverActions/fetchRecipeDataFromSupabase";
 import RecipeCard from "@/components/RecipeCard";
 import { Recipe } from "@/types/Recipe";
-import { Link } from "lucide-react";
 
 export default async function Home() {
-  const data = await fetchRecipeData();
-  const recipes = data.COOKRCP01.row.slice(0, 5); //5개만 썰어와
+  const data = await fetchRecipeDbData();
+  const recipes = data?.slice(0, 10);
 
   return (
     <div className="p-6">
-      <Link href="/login">로그인페이지</Link>
-      <h1 className="text-2xl font-bold mb-4">요리 5개만 불러와보자</h1>
+      <h1 className="text-2xl font-bold mb-4">요리 10개만 불러와보자</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {recipes.map((recipe: Recipe, index: number) => (
-          <RecipeCard
-            key={index}
-            recipe={{
-              id: recipe.id,
-              RCP_NM: recipe.RCP_NM,
-              ATT_FILE_NO_MAIN: recipe.ATT_FILE_NO_MAIN,
-              ATT_FILE_NO_MK: recipe.ATT_FILE_NO_MK
-            }}
-          />
-        ))}
+        {recipes?.map((recipe: Recipe) => <RecipeCard key={recipe.post_id} recipe={recipe} />)}
       </div>
     </div>
   );
