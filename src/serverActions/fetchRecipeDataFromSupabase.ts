5733dcdabb58ea45a0554488fe06f2e400923179
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-10-26 17:34:48 +0900

    fix : TEXT_TABEL 데이터 불러오는 로직 수정

--- src/serverActions/fetchRecipeDataFromSupabase.ts
+++ src/serverActions/fetchRecipeDataFromSupabase.ts
@@ -1,17 +1,13 @@
 import { supabase } from "@/supabase/supabase";
 
-export const fetchRecipeDataFromSupabase = async (postId?: string) => {
-  const { data, error } = await supabase.from("TEST_TABLE").select("*").eq("post_id", postId);
-  console.log(postId);
+export const fetchRecipeDbData = async () => {
+  const { data, error } = await supabase.from("TEST_TABLE").select("*");
+
   if (error) {
-    console.log("레시피 데이터 불러오기 중 오류 발생", error.message);
-    throw error;
+    console.error("레시피 데이터 오류", error);
+    return;
   }
 
-  if (!data || data.length === 0) {
-    console.log("해당 post_id에 맞는 데이터가 없습니다.");
-    return null; // 또는 빈 객체, 사용자 알림 등 적절한 반환 처리
-  }
-  console.log(data);
+  // console.log("레시피 데이터", data);
   return data;
 };
