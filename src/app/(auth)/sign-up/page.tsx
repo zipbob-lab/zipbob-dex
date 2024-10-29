2274d6bdedcbca72abd28b94260914a41a91cd32
slppills <sunchaehoon2@naver.com>
2024-10-29 15:16:38 +0900

    add : 회원가입 페이지

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -3,7 +3,6 @@ import SignUpForm from "@/components/authPage/Form/SignUpForm";
 const SignupPage = () => {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-[2rem]">회원가입</h1>
       <SignUpForm />
     </div>
   );
