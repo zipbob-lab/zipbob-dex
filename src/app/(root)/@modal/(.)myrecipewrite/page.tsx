081662718da46a563b720f577c4da517cd8cbbf6
star1024cd <1star1024cd@gmail.com>
2024-11-01 20:38:52 +0900

    fix : 레시피 리스트에서 모달 전파 막기

--- src/app/(root)/@modal/(.)myrecipewrite/page.tsx
+++ src/app/(root)/@modal/(.)myrecipewrite/page.tsx
@@ -5,7 +5,10 @@ import MyRecipeWrite from "../../myrecipewrite/page";
 const RecipeWriteModal = () => {
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-      <div className="bg-white p-6 rounded-lg w-full max-w-5xl" onClick={(e) => e.stopPropagation()}>
+      <div
+        className="bg-white p-6 rounded-lg w-full max-w-5xl max-h-[80vh] overflow-y-auto"
+        onClick={(e) => e.stopPropagation()}
+      >
         <MyRecipeWrite />
       </div>
     </div>
