f812d935f3a7eb7650ed42d4fd51b8b8c921efea
star1024cd <1star1024cd@gmail.com>
2024-11-03 07:12:30 +0900

    refactor : 레시피 작성 재료필드/레시피정보필드 컴포넌트 분리

---
+++ src/components/myrecipewrite/RecipeInfoFields.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useFormContext } from "react-hook-form";
+
+const RecipeInfoFields = () => {
+  const { register,control } = useFormContext();
+  return (
+    <div>
+      <div className="flex mb-5 gap-10">
+        <label className="font-bold">카테고리</label>
+        <select {...register("recipeMethod", { required: true })}>
+          <option value="boil">끓이기</option>
+          <option value="roast">굽기</option>
+          <option value="frying">튀기기</option>
+          <option value="steaming">찌기</option>
+          <option value="stirfry">볶기</option>
+          <option value="other">기타</option>
+        </select>
+        <select {...register("recipeType", { required: true })}>
+          <option value="bob">밥</option>
+          <option value="soup">국&찌개</option>
+          <option value="banchan">반찬</option>
+          <option value="onefood">일품</option>
+          <option value="desert">후식</option>
+          <option value="other">기타</option>
+        </select>
+      </div>
+      <div className="flex mb-5 gap-10">
+        <label className="font-bold">레시피 제목</label>
+        <input placeholder="예)10분이면 완성하는 바질 로제 파스타" {...register("recipeTitle", { required: true })} />
+      </div>
+      <div className="flex mb-5 gap-10">
+        <label className="font-bold">요리 소개</label>
+        <textarea
+          className="resize-none"
+          placeholder="레시피의 탄생배경, 특징을 적어주세요."
+          {...register("recipeDescription", { required: true })}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default RecipeInfoFields;
