e434efec2a007dd632993086dc9d4682c6053a40
slppills <sunchaehoon2@naver.com>
2024-11-02 20:02:55 +0900

    fix : 메인페이지 타입

--- src/types/main.ts
+++ src/types/main.ts
@@ -6,3 +6,24 @@ export type PostRank = {
   post_count: number;
   post_id: string;
 };
+
+export type RecentCommentCardProps = {
+  comment: {
+    comment: string;
+    post_id: string;
+    user_id: string;
+    created_at: string;
+  };
+};
+
+export type RecipeCardProps = {
+  post: {
+    post_id: string;
+    recipe_img_done: string;
+    recipe_title: string;
+    like_count: number;
+    scrap_count: number;
+    recipe_level: string;
+    user_id: string;
+  };
+};
