1c389555b3a8ce071679a1f5a423174fd7bd4345
slppills <sunchaehoon2@naver.com>
2024-11-04 22:43:43 +0900

    fix : InputField 이름 수정

--- src/app/(root)/myrecipewrite/page.tsx
+++ src/app/(root)/myrecipewrite/page.tsx
@@ -1,11 +1,11 @@
-import InputAA from "@/components/myrecipewrite/InputField";
+import InputField from "@/components/myrecipewrite/InputField";
 import { Suspense } from "react";
 
 const MyRecipeWrite = () => {
   return (
     <div>
       <Suspense fallback={<div>Loading</div>}>
-        <InputAA />
+        <InputField />
       </Suspense>
     </div>
   );
