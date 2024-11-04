55db5f8c696aab9175163c7c183d26d89554022f
jeongol <jungcode127@gmail.com>
2024-11-04 11:54:49 +0900

    fix: Image 컴포넌트 사용

--- src/components/fridgeList/ListCard.tsx
+++ src/components/fridgeList/ListCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { Recipe } from "@/types/Recipe";
+import Image from "next/image";
 
 interface RecipeCardProps {
   recipe: Recipe;
@@ -11,7 +12,7 @@ const RecipeCard: React.FC<RecipeCardProps> = ({ recipe }) => {
     <li key={recipe.post_id}>
       <Link href={`/myrecipedetail/${recipe.post_id}`}>
         <h2>{recipe.recipe_title}</h2>
-        <img src={recipe.recipe_img_done} alt="이미지 없음" />
+        <Image src={recipe.recipe_img_done} alt="이미지 없음" />
         <p>난이도: {recipe.recipe_level}</p>
         <p>좋아요: {recipe.like_count}</p>
         <p>스크랩: {recipe.scrap_count}</p>
