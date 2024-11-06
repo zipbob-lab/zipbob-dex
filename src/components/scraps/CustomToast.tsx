2e22e153e39248da26ebd07b46ee63e7b0579ee8
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-11-06 16:08:44 +0900

    style : 스크랩 확인 창 위치 수정

--- src/components/scraps/CustomToast.tsx
+++ src/components/scraps/CustomToast.tsx
@@ -22,7 +22,7 @@ const CustomToast: React.FC<CustomToastProps> = ({ message, onMove }) => {
   };
 
   return (
-    <div className="fixed bottom-5 z-50 flex h-11 w-1/2 items-center justify-between space-x-4 rounded-xl bg-[#454443] p-4 text-white">
+    <div className="fixed bottom-10 left-1/2 z-50 flex h-11 w-1/2 max-w-md -translate-x-1/2 items-center justify-between space-x-4 rounded-xl bg-[#454443] p-4 text-white">
       <p>{message}</p>
       <button onClick={handleRedirect} className="text-body-16 text-Primary-300">
         스크랩 이동
