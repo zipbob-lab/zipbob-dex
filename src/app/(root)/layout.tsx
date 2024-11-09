4d21776ad296ad1f11a245eae7fbb498b9c3bb08
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-11-09 17:06:27 +0900

    refac : 스크랩 모달 -> createPortal 모달로 수정 작업

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
       <ProfileDropbox />
       <div className="flex-1">{children}</div>
       {modal}
+      <div id="modal-root"></div>
       <Footer />
     </div>
   );
