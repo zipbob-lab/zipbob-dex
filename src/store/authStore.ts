913403eb917650a6e34210aae4ee8253a9f40ea6
slppills <sunchaehoon2@naver.com>
2024-11-09 17:05:27 +0900

    fix : auth state 추가

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -4,14 +4,18 @@ import { create } from "zustand";
 type AuthState = {
   isLoggedIn: boolean;
   setIsLoggedIn: (loggedIn: boolean) => void;
+  userId: string | null;
+  setUserId: (userId: string) => void;
   logout: () => Promise<void>;
 };
 
 export const useAuthStore = create<AuthState>((set) => ({
   isLoggedIn: false,
+  userId: null,
   setIsLoggedIn: (loggedIn) => set({ isLoggedIn: loggedIn }),
+  setUserId: (id) => set({ userId: id }),
   logout: async () => {
     await browserClient.auth.signOut();
-    set({ isLoggedIn: false });
+    set({ isLoggedIn: false, userId: null });
   }
 }));
