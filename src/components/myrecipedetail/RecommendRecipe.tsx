08982f9b47031ea8649d33094c1a66cf17a3f9ec
star1024cd <1star1024cd@gmail.com>
2024-11-15 04:36:02 +0900

    feat : 추천 레시피 캐러셀 추가

--- src/components/myrecipedetail/RecommendRecipe.tsx
+++ src/components/myrecipedetail/RecommendRecipe.tsx
@@ -3,11 +3,13 @@
 import browserClient from "@/supabase/client";
 import { useQuery } from "@tanstack/react-query";
 import RecipeCard2 from "../mainPage/RecipeCard2";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 const RecommendRecipe = () => {
 
   const fetchRamdomRecipe = async () => {
-    const { data, error } = await browserClient.from("MY_RECIPE_TABLE").select("*").not('recipe_seq', 'is', null).order("recipe_seq", { ascending: false }).limit(4);
+    const { data, error } = await browserClient.from("MY_RECIPE_TABLE").select("*").not('recipe_seq', 'is', null).order("recipe_seq", { ascending: false }).limit(10);
 
     if (error) {
       console.error("추천 레시피를 불러오는 과정에서 에러 발생" + error);
@@ -39,8 +41,14 @@ const RecommendRecipe = () => {
       <h1 className="py-[0.75rem] text-heading-24 text-Gray-900">
         이 레시피는 어때요?
       </h1>
-      <div className="mt-[1rem] flex gap-[1rem] overflow-x-auto">
-        {posts?.map((post) => <RecipeCard2 key={post.id} post={post} />)}
+      <div className="mt-[1rem] flex gap-[1rem]">
+        <Swiper spaceBetween={1} slidesPerView={4}>      
+          {posts?.map((post) => (
+            <SwiperSlide key={post.id}>
+              <RecipeCard2 post={post} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
