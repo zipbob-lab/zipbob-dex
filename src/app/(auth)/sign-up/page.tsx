9011b0518aa0496cf36a3da25719546f97c3eb4d
slppills <sunchaehoon2@naver.com>
2024-11-18 17:02:58 +0900

    style : 회원가입 페이지 반응형 작업

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -3,7 +3,7 @@ import SignUpForm from "@/components/authPage/Form/SignUpForm";
 const SignupPage = () => {
   return (
     <div className="-mt-3 flex h-screen flex-col items-center justify-center">
-      <div className="flex w-[520px] flex-col items-center rounded-3xl px-10 py-[3.5rem] shadow-[0px_4px_20px_0px_rgba(154,130,102,0.1)]">
+      <div className="flex w-[20.9rem] flex-col items-center md:w-[27.5rem] lg:w-[32.5rem] lg:rounded-3xl lg:px-[2.5rem] lg:py-[3.5rem] lg:shadow-[0px_4px_20px_0px_rgba(154,130,102,0.1)]">
         <SignUpForm />
       </div>
     </div>
