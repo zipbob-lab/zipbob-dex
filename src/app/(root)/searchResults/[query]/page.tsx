cf6d8e0dacd25cb0453dcb43451c6f51e3f48946
jeongol <jungcode127@gmail.com>
2024-11-19 16:26:23 +0900

    add: 메타데이터 설정 추가

--- src/app/(root)/searchResults/[query]/page.tsx
+++ src/app/(root)/searchResults/[query]/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import SearchResults from "@/components/searchResultsPage/SearchResults";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "검색결과",
+  description: "단어를 입력해 원하는 레시피를 찾아 볼수 있습니다."
+};
 
 const SearchResultPage = () => {
   return (
