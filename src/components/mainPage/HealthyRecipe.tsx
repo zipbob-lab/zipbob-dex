5c28bb537e5055f388817b3781a7df5fdd6dcc1a
slppills <sunchaehoon2@naver.com>
2024-11-20 11:02:17 +0900

    fix : pending 중에 로딩스피너 사용

--- src/components/mainPage/HealthyRecipe.tsx
+++ src/components/mainPage/HealthyRecipe.tsx
@@ -5,6 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 import RecipeCard from "./RecipeCard";
 import { useState } from "react";
 import Pagination from "../common/Pagination";
+import LoadingSpinner from "../common/LoadingSpinner";
 
 const HealthyRecipe = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -33,10 +34,6 @@ const HealthyRecipe = () => {
     staleTime: 0
   });
 
-  if (isPostPending) {
-    return <div>저칼로리 레시피를 가져오는중입니다</div>;
-  }
-
   if (isPostError) {
     return <div>저칼로리 레시피를 가져오는 도중 에러가 발생했습니다</div>;
   }
@@ -48,8 +45,8 @@ const HealthyRecipe = () => {
         <p className="mt-3 text-body-14 md:text-body-16 xl:mt-4 xl:text-body-18">
           집밥도감이 추천하는 건강한 레시피에 도전해 보세요!
         </p>
-        <div className="mt-[1.75rem] grid grid-cols-2 place-items-center gap-x-3 gap-y-4 md:mt-[2rem] md:flex md:justify-center xl:mt-[3.75rem] xl:gap-[1rem]">
-          {posts?.map((post) => <RecipeCard key={post.id} post={post} />)}
+        <div className="mt-[1.75rem] grid grid-cols-2 place-items-center gap-x-3 gap-y-4 md:mt-[2rem] md:flex md:justify-between xl:mt-[3.75rem] xl:gap-[1rem]">
+          {isPostPending ? <LoadingSpinner /> : posts?.map((post) => <RecipeCard key={post.id} post={post} />)}
         </div>
         <div className="mt-[1.75rem] flex items-center justify-center md:mt-[2rem] xl:mt-[3rem]">
           <Pagination
--- src/components/mainPage/RecentRecipe.tsx
+++ src/components/mainPage/RecentRecipe.tsx
@@ -5,6 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 import RecipeCard from "./RecipeCard";
 import { useState } from "react";
 import Pagination from "../common/Pagination";
+import LoadingSpinner from "../common/LoadingSpinner";
 
 const RecentRecipe = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -33,10 +34,6 @@ const RecentRecipe = () => {
     staleTime: 0
   });
 
-  if (isPostPending) {
-    return <div>좋아요 랭킹을 가져오는중입니다</div>;
-  }
-
   if (isPostError) {
     return <div>좋아요 랭킹을 가져오는 도중 에러가 발생했습니다</div>;
   }
@@ -48,8 +45,8 @@ const RecentRecipe = () => {
         <p className="mt-3 text-body-14 md:text-body-16 xl:mt-4 xl:text-body-18">
           집밥도감 유저들이 올려준 레시피에 도전해 보세요!
         </p>
-        <div className="mt-[1.75rem] grid grid-cols-2 place-items-center gap-x-3 gap-y-4 md:mt-[2rem] md:flex md:justify-center xl:mt-[3.75rem] xl:gap-[1rem]">
-          {posts?.map((post) => <RecipeCard key={post.id} post={post} />)}
+        <div className="mt-[1.75rem] grid grid-cols-2 place-items-center gap-x-3 gap-y-4 md:mt-[2rem] md:flex md:justify-between xl:mt-[3.75rem] xl:gap-[1rem]">
+          {isPostPending ? <LoadingSpinner /> : posts?.map((post) => <RecipeCard key={post.id} post={post} />)}
         </div>
         <div className="mt-[1.75rem] flex items-center justify-center md:mt-[2rem] xl:mt-[3rem]">
           <Pagination
